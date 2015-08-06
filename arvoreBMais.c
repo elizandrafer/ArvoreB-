8137b09c2aca1bc83d0bfac27c448dd737ef8038
Elizandra <efr@icomp.ufam.edu.br>
2015-08-06 15:28:59 -0400

    Teste

---
+++ arvoreBMais.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//**verificar a ordem D de modo que possa ser dinâmico.
+#define D 10
+
+//Estrutura da Arvore B+
+typedef struct paginaDaArvore{
+
+  //quantidade de chaves na páginaRaiz
+  long int totalChaves;
+
+  //vetor de chaves inteiras
+  int chaves[D-1];
+
+  //ponteiro para as paginas filhas
+  struct paginaFilha[D];   
+
+}paginaDaArvore;
+
+
+
+
+
+
+void main(){
+
+
+}
