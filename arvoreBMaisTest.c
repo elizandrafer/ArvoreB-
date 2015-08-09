de6c90e922302128c5ad1bae2e675c844219330c
Nadny Dantas <nadny.dantas@gmail.com>
2015-08-09 01:09:34 -0400

    Menu, Inserção e Busca funcionando

--- arvoreBMaisTest.c
+++ arvoreBMaisTest.c
@@ -65,29 +65,29 @@ typedef struct configuracaoArvore {
 }configuracaoArvore;
 
 paginaInterna* criarPagina(void) {
-  paginaInterna *no = malloc(sizeof(*no));
-  assert(no != NULL);
-  no->tipo = 1;  //não é folha
-  memset(no->chaves, 0 , (maxOrdem-1) * sizeof(int));
-  memset(no->subPtr, 0, (maxOrdem) * sizeof(noDaArvore *));
-  no->pai = NULL;
-  no->proximo = NULL;
-  no->filhos = 0;
-  
-  return no;
+    paginaInterna *no = malloc(sizeof(*no));
+    assert(no != NULL);
+    no->tipo = 1;  //não é folha
+    memset(no->chaves, 0 , (maxOrdem-1) * sizeof(int));
+    memset(no->subPtr, 0, (maxOrdem) * sizeof(noDaArvore *));
+    no->pai = NULL;
+    no->proximo = NULL;
+    no->filhos = 0;
+    
+    return no;
 };
 
 paginaFolha* criarFolha(void) {
-  paginaFolha *folha = malloc(sizeof(*folha));
-  assert(folha != NULL);
-  folha->tipo = 0;     //é folha
-  memset(folha->chaves, 0, maxRegistro * sizeof(int));
-  memset(folha->dados, 0, maxRegistro * sizeof(int));
-  folha->pai = NULL;
-  folha->proximo = NULL;
-  folha->registros = 0;
+    paginaFolha *folha = malloc(sizeof(*folha));
+    assert(folha != NULL);
+    folha->tipo = 0;     //é folha
+    memset(folha->chaves, 0, maxRegistro * sizeof(int));
+    memset(folha->dados, 0, maxRegistro * sizeof(int));
+    folha->pai = NULL;
+    folha->proximo = NULL;
+    folha->registros = 0;
 
-  return folha;
+    return folha;
 }
 
 arvoreBMais arvore;
@@ -101,22 +101,22 @@ void removerPagina(paginaInterna *no){
 }
 
 int buscaBinaria(int *v, int tam, int valor) {
-  int meio, inicio = -1, fim = tam;
-  
-  while(inicio + 1 < fim) {
-    meio = inicio + (fim - inicio)/2;  
-    if(valor > v[meio]) {
-      inicio = meio;
+    int meio, inicio = -1, fim = tam;
+    
+    while(inicio + 1 < fim) {
+        meio = inicio + (fim - inicio)/2;  
+        if(valor > v[meio]) {
+            inicio = meio;
+        }
+        else if (valor < v[meio]) {
+            fim = meio;
+        }
     }
-    else if (valor < v[meio]) {
-      fim = meio;
+    if (fim >= tam || v[fim] != valor) {
+        return -fim - 1;
+    } else {
+        return fim;
     }
-  }
-  if (fim >= tam || v[fim] != valor) {
-    return -fim - 1;
-  } else {
-    return fim;
-  }
 }
 
 int buscarNaArvore(arvoreBMais *arvore, int chave)
@@ -166,117 +166,117 @@ int inserirNaPagina(arvoreBMais *arvore, paginaInterna *pagina, noDaArvore *noAr
 
     if (pagina->filhos == arvore->ordem) {
                 
-            split = (arvore->ordem + 1) / 2;
+        split = (arvore->ordem + 1) / 2;
                 
-            sibling = criarPagina();
-            sibling->proximo = pagina->proximo;
-            pagina->proximo = sibling;
-            pagina->filhos = split + 1;
+        sibling = criarPagina();
+        sibling->proximo = pagina->proximo;
+        pagina->proximo = sibling;
+        pagina->filhos = split + 1;
                 
-            if (auxInserir < split) {
-                i = split - 1;
-                j = 0;
-                split_chave = pagina->chaves[i];
-                sibling->subPtr[j] = pagina->subPtr[i+1];
+        if (auxInserir < split) {
+            i = split - 1;
+            j = 0;
+            split_chave = pagina->chaves[i];
+            sibling->subPtr[j] = pagina->subPtr[i+1];
+            pagina->subPtr[i+1]->pai = sibling;
+            i++;
+                        
+            while (i < arvore->ordem - 1) {
+                sibling->chaves[j] = pagina->chaves[i];
+                sibling->subPtr[j+1] = pagina->subPtr[i+1];
                 pagina->subPtr[i+1]->pai = sibling;
                 i++;
-                        
-                while (i < arvore->ordem - 1) {
-                    sibling->chaves[j] = pagina->chaves[i];
-                    sibling->subPtr[j+1] = pagina->subPtr[i+1];
-                    pagina->subPtr[i+1]->pai = sibling;
-                    i++;
-                    j++;
-                }
+                j++;
+            }
 
-                sibling->filhos = j + 1;
+            sibling->filhos = j + 1;
                                                 
-                for (i = pagina->filhos - 1; i > auxInserir; i--) {
-                    pagina->chaves[i] = pagina->chaves[i-1];
-                    pagina->subPtr[i+1] = pagina->subPtr[i];
-                }
-
-                pagina->chaves[i] = chave;
-                pagina->subPtr[i+1] = noArv;
-            } else if (auxInserir == split) {
-                i = split;
-                j = 0;
-                split_chave = chave;
-                sibling->subPtr[j] = noArv;
-                noArv->pai = sibling;
-                i++;
-
-                while(i < arvore->ordem - 1){
-                    sibling->chaves[j] = pagina->chaves[i];
-                    sibling->subPtr[j+1] = pagina->subPtr[i+1];
-                    pagina->subPtr[i+1]->pai = sibling;
-                    i++;
-                    j++;
-                }
-
-                sibling->filhos = j + 1;
-            } else {
-                i = split;
-                j = 0;
-                split_chave = pagina->chaves[i];
-                sibling->subPtr[j] = pagina->subPtr[i+1];
-                pagina->subPtr[i+1]->pai = sibling;
-                i++;
-                while (i < arvore->ordem - 1) {
-                    if (j != auxInserir - split) {
-                        sibling->chaves[j] = pagina->chaves[i];
-                        sibling->subPtr[j+1] = pagina->subPtr[i+1];
-                        pagina->subPtr[i+1]->pai = sibling;
-                        i++;
-                    }
-                    j++;
-                }
-                        
-                if (j > auxInserir - split) {
-                    sibling->filhos = j+1;
-                } else {
-                    sibling->filhos = auxInserir - split+1;
-                }
-                        
-                j = auxInserir - split - 1;
-                sibling->chaves[j] = chave;
-                sibling->subPtr[j+1] = noArv;
-                noArv->pai = sibling;
-        }
-    } else {
             for (i = pagina->filhos - 1; i > auxInserir; i--) {
                 pagina->chaves[i] = pagina->chaves[i-1];
                 pagina->subPtr[i+1] = pagina->subPtr[i];
             }
+
             pagina->chaves[i] = chave;
             pagina->subPtr[i+1] = noArv;
-            pagina->filhos++;
+        } else if (auxInserir == split) {
+            i = split;
+            j = 0;
+            split_chave = chave;
+            sibling->subPtr[j] = noArv;
+            noArv->pai = sibling;
+            i++;
+
+            while(i < arvore->ordem - 1){
+                sibling->chaves[j] = pagina->chaves[i];
+                sibling->subPtr[j+1] = pagina->subPtr[i+1];
+                pagina->subPtr[i+1]->pai = sibling;
+                i++;
+                j++;
+            }
+
+            sibling->filhos = j + 1;
+        } else {
+            i = split;
+            j = 0;
+            split_chave = pagina->chaves[i];
+            sibling->subPtr[j] = pagina->subPtr[i+1];
+            pagina->subPtr[i+1]->pai = sibling;
+            i++;
+            while (i < arvore->ordem - 1) {
+                if (j != auxInserir - split) {
+                    sibling->chaves[j] = pagina->chaves[i];
+                    sibling->subPtr[j+1] = pagina->subPtr[i+1];
+                    pagina->subPtr[i+1]->pai = sibling;
+                    i++;
+                }
+                j++;
+            }
+                        
+            if (j > auxInserir - split) {
+                sibling->filhos = j+1;
+            } else {
+                sibling->filhos = auxInserir - split+1;
+            }
+                        
+            j = auxInserir - split - 1;
+            sibling->chaves[j] = chave;
+            sibling->subPtr[j+1] = noArv;
+            noArv->pai = sibling;
+      }
+    } else {
+        for (i = pagina->filhos - 1; i > auxInserir; i--) {
+            pagina->chaves[i] = pagina->chaves[i-1];
+            pagina->subPtr[i+1] = pagina->subPtr[i];
+        }
+        pagina->chaves[i] = chave;
+        pagina->subPtr[i+1] = noArv;
+        pagina->filhos++;
     }
 
     if (split) {
-            paginaInterna *pai = pagina->pai;
-            if (pai == NULL) {                        
-                if (++nivel >= arvore->nivel) {
-                    fprintf(stderr, "!!Panic: Nivel exceeded, please expand the arvore nivel, non-leaf ordem or leaf registros for element capacity!\n");
-                    pagina->proximo = sibling->proximo;
-                    removerPagina(sibling);
-                    return -1;
-                }
-                        
-                pai = criarPagina();
-                pai->chaves[0] = split_chave;
-                pai->subPtr[0] = (noDaArvore*)pagina;
-                pai->subPtr[1] = (noDaArvore*)sibling;
-                pai->filhos = 2;
-                    
-                arvore->raiz = (noDaArvore*)pai;
-                arvore->cabeca[nivel] = (noDaArvore*)pai;
-                pagina->pai = pai;
-                sibling->pai = pai;
-            } else {  
-                sibling->pai = pai;
-                return inserirNaPagina(arvore, pai, (noDaArvore*)sibling, split_chave, nivel+1);
+        paginaInterna *pai = pagina->pai;
+        if (pai == NULL) {                        
+            if (++nivel >= arvore->nivel) {
+                fprintf(stderr, "!!Panic: Nivel exceeded, please expand the arvore nivel, non-leaf ordem or leaf registros for element capacity!\n");
+                pagina->proximo = sibling->proximo;
+                removerPagina(sibling);
+                return -1;
             }
+                        
+            pai = criarPagina();
+            pai->chaves[0] = split_chave;
+            pai->subPtr[0] = (noDaArvore*)pagina;
+            pai->subPtr[1] = (noDaArvore*)sibling;
+            pai->filhos = 2;
+                    
+            arvore->raiz = (noDaArvore*)pai;
+            arvore->cabeca[nivel] = (noDaArvore*)pai;
+            pagina->pai = pai;
+            sibling->pai = pai;
+        } else {  
+            sibling->pai = pai;
+            return inserirNaPagina(arvore, pai, (noDaArvore*)sibling, split_chave, nivel+1);
+        }
     }
 
     return 0;
@@ -284,123 +284,123 @@ int inserirNaPagina(arvoreBMais *arvore, paginaInterna *pagina, noDaArvore *noAr
 
 int inserirNaFolha(arvoreBMais *arvore, paginaFolha *folha, int chave, int dado)
 {
-        int i, j, split = 0;
-        paginaFolha *sibling;
+    int i, j, split = 0;
+    paginaFolha *sibling;
 
-        int auxInserir = buscaBinaria(folha->chaves, folha->registros, chave);
-        if (auxInserir >= 0) {
-                return -1;
-        }
-        auxInserir = -auxInserir - 1;
+    int auxInserir = buscaBinaria(folha->chaves, folha->registros, chave);
+    if (auxInserir >= 0) {
+            return -1;
+    }
+    auxInserir = -auxInserir - 1;
 
-        if (folha->registros == arvore->registros) {
-                split = (arvore->registros + 1) / 2;
-                sibling = criarFolha();
-                sibling->proximo = folha->proximo;
-                folha->proximo = sibling;
-                folha->registros = split;
+    if (folha->registros == arvore->registros) {
+            split = (arvore->registros + 1) / 2;
+            sibling = criarFolha();
+            sibling->proximo = folha->proximo;
+            folha->proximo = sibling;
+            folha->registros = split;
                 
-                if (auxInserir < split) {
-                        for (i = split - 1, j = 0; i < arvore->registros; i++, j++) {
-                                sibling->chaves[j] = folha->chaves[i];
-                                sibling->dados[j] = folha->dados[i];
-                        }
-                        sibling->registros = j;
-                        
-                        for (i = folha->registros; i > auxInserir; i--) {
-                                folha->chaves[i] = folha->chaves[i-1];
-                                folha->dados[i] = folha->dados[i-1];
-                        }
-                        folha->chaves[i] = chave;
-                        folha->dados[i] = dado;
-                } else {
-                        i = split, j = 0;
-                        while (i < arvore->registros) {
-                                if (j != auxInserir - split) {
-                                        sibling->chaves[j] = folha->chaves[i];
-                                        sibling->dados[j] = folha->dados[i];
-                                        i++;
-                                }
-                                j++;
-                        }
-
-                        if (j > auxInserir - split) {
-                                sibling->registros = j;
-                        } else {
-                                sibling->registros = auxInserir - split + 1;
-                        }
-
-                        j = auxInserir - split;
-                        sibling->chaves[j] = chave;
-                        sibling->dados[j] = dado;
+            if (auxInserir < split) {
+                for (i = split - 1, j = 0; i < arvore->registros; i++, j++) {
+                    sibling->chaves[j] = folha->chaves[i];
+                            sibling->dados[j] = folha->dados[i];
                 }
-        } else {
+                sibling->registros = j;
+                        
                 for (i = folha->registros; i > auxInserir; i--) {
-                        folha->chaves[i] = folha->chaves[i-1];
-                        folha->dados[i] = folha->dados[i-1];
+                    folha->chaves[i] = folha->chaves[i-1];
+                    folha->dados[i] = folha->dados[i-1];
+                }
+                folha->chaves[i] = chave;
+                folha->dados[i] = dado;
+            } else {
+                i = split, j = 0;
+                while (i < arvore->registros) {
+                    if (j != auxInserir - split) {
+                        sibling->chaves[j] = folha->chaves[i];
+                        sibling->dados[j] = folha->dados[i];
+                        i++;
+                    }
+                    j++;
+                }
+
+                if (j > auxInserir - split) {
+                    sibling->registros = j;
+                } else {
+                    sibling->registros = auxInserir - split + 1;
+                }
+
+                j = auxInserir - split;
+                sibling->chaves[j] = chave;
+                sibling->dados[j] = dado;
+            }
+    } else {
+                for (i = folha->registros; i > auxInserir; i--) {
+                    folha->chaves[i] = folha->chaves[i-1];
+                    folha->dados[i] = folha->dados[i-1];
                 }
                 folha->chaves[i] = chave;
                 folha->dados[i] = dado;
                 folha->registros++;
-        }
+    }
 
-        if (split) {
-                paginaInterna *pai = folha->pai;
-                if (pai == NULL) {
-                        pai = criarPagina();
-                        pai->chaves[0] = sibling->chaves[0];
-                        pai->subPtr[0] = (noDaArvore*)folha;
-                        pai->subPtr[1] = (noDaArvore*)sibling;
-                        pai->filhos = 2;
+    if (split) {
+        paginaInterna *pai = folha->pai;
+        if (pai == NULL) {
+            pai = criarPagina();
+            pai->chaves[0] = sibling->chaves[0];
+            pai->subPtr[0] = (noDaArvore*)folha;
+            pai->subPtr[1] = (noDaArvore*)sibling;
+            pai->filhos = 2;
                         
-                        arvore->raiz = (noDaArvore*)pai;
-                        arvore->cabeca[1] = (noDaArvore*)pai;
-                        folha->pai = pai;
-                        sibling->pai = pai;
-                } else {
-                        sibling->pai = pai;
-                        return inserirNaPagina(arvore, pai, (noDaArvore*)sibling, sibling->chaves[0], 1);
-                }
+            arvore->raiz = (noDaArvore*)pai;
+            arvore->cabeca[1] = (noDaArvore*)pai;
+            folha->pai = pai;
+            sibling->pai = pai;
+        } else {
+            sibling->pai = pai;
+            return inserirNaPagina(arvore, pai, (noDaArvore*)sibling, sibling->chaves[0], 1);
         }
+    }
 
-        return 0;
+    return 0;
 }
 
 int inserirNaArvore(arvoreBMais *arvore, int chave, int dado)
 {
-        int i;
-        noDaArvore *no = arvore->raiz;
-        paginaInterna *naoFolha;
-        paginaFolha *folha, *raiz;
+    int i;
+    noDaArvore *no = arvore->raiz;
+    paginaInterna *naoFolha;
+    paginaFolha *folha, *raiz;
 
-        while (no != NULL) {
-                switch (no->tipo) {
-                case 0:
-                        folha = (paginaFolha*)no;
-                        return inserirNaFolha(arvore, folha, chave, dado);  
-                case 1:
-                        naoFolha = (paginaInterna*)no;
-                        i = buscaBinaria(naoFolha->chaves, naoFolha->filhos - 1, chave);
-                        if (i >= 0) {
-                                no = naoFolha->subPtr[i+1];
-                        } else {
-                                i = -i - 1;
-                                no = naoFolha->subPtr[i];
-                        }
-                        break;
-                default:
-                        assert(0);
-                }
+    while (no != NULL) {
+        switch (no->tipo) {
+        case 0:
+            folha = (paginaFolha*)no;
+            return inserirNaFolha(arvore, folha, chave, dado);  
+        case 1:
+            naoFolha = (paginaInterna*)no;
+            i = buscaBinaria(naoFolha->chaves, naoFolha->filhos - 1, chave);
+            if (i >= 0) {
+                no = naoFolha->subPtr[i+1];
+            } else {
+                i = -i - 1;
+                no = naoFolha->subPtr[i];
+            }
+            break;
+        default:
+                assert(0);
         }
+    }
 
-        raiz = criarFolha();
-        raiz->chaves[0] = chave;
-        raiz->dados[0] = dado;
-        raiz->registros = 1;
-        arvore->cabeca[0] = (noDaArvore*)raiz;
-        arvore->raiz = (noDaArvore*)raiz;
+    raiz = criarFolha();
+    raiz->chaves[0] = chave;
+    raiz->dados[0] = dado;
+    raiz->registros = 1;
+    arvore->cabeca[0] = (noDaArvore*)raiz;
+    arvore->raiz = (noDaArvore*)raiz;
 
-        return 0;
+    return 0;
 }
 
 
